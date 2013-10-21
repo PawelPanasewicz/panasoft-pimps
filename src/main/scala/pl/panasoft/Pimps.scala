892aad7a3cdf77fa3ca4233c2dbb8c11d0db2312
Paweł Panasewicz <pawel.panasewicz@panasoft.pl>
2013-10-21 20:02:20 +0200

    corssbuild, tests run only in scala version 2.10.x

--- src/main/scala/pl/panasoft/Pimps.scala
+++ src/main/scala/pl/panasoft/Pimps.scala
@@ -1,47 +1,47 @@
 package pl.panasoft
 
-trait Pimps {
-
-  implicit class AndThenOps[A](self: => A) {
-
-    def andThen[X](f: => X) = {
-      self;
-      f
-    }
-
-    /**
-     * alias for `andThen`
-     */
-    def andThenPerform[X] = andThen[X] _
+class AndThenOps[A](self: => A) {
+  def andThen[X](f: => X) = {
+    self;
+    f
   }
 
-  implicit class UMapOps[A](self: => A) {
-    def uMap[X](f: A => X) = f(self)
-  }
-
-  implicit class ModifyOps[A](self: => A) {
-    def modify(f: A => _): A = {
-      val onceCalledSelf = self
-      f(onceCalledSelf) andThen onceCalledSelf
-    }
-  }
-
-  implicit class AssertionOps[A](self: => A) {
-
-    def assertThat(test: A => Boolean, errMessage: String = ""): A = {
-      val onceCalledSelf = self
-      if (!test(onceCalledSelf)) throw new AssertionError(errMessage)
-      else onceCalledSelf
-    }
-
-    def throwIf[E <: RuntimeException](test: A => Boolean)(e: => E): A = {
-      val onceCalledSelf = self
-      if (test(onceCalledSelf)) throw e
-      else onceCalledSelf
-    }
-  }
-
+  /**
+   * alias for `andThen`
+   */
+  def andThenPerform[X] = andThen[X] _
 }
 
+class UMapOps[A](self: => A) {
+  def uMap[X](f: A => X) = f(self)
+}
 
-object Pimps extends Pimps
+class ModifyOps[A](self: => A) {
+  def modify[B](f: A => B): A = {
+    val onceCalledSelf = self
+    f(onceCalledSelf)
+    onceCalledSelf
+  }
+}
+
+class AssertionOps[A](self: => A) {
+
+  def assertThat(test: A => Boolean, errMessage: String = ""): A = {
+    val onceCalledSelf = self
+    if (!test(onceCalledSelf)) throw new AssertionError(errMessage)
+    else onceCalledSelf
+  }
+
+  def throwIf[E <: RuntimeException](test: A => Boolean)(e: => E): A = {
+    val onceCalledSelf = self
+    if (test(onceCalledSelf)) throw e
+    else onceCalledSelf
+  }
+}
+
+object Pimps {
+  implicit def toAndThenOps[A](a: A) = new AndThenOps(a)
+  implicit def toUMapOps[A](a: A) = new UMapOps(a)
+  implicit def toModifyOps[A](a: A) = new ModifyOps(a)
+  implicit def toAssertionOps[A](a: A) = new AssertionOps(a)
+}
